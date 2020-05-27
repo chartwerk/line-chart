469a5a751ca1bab76b22ee5e86e0316b5254e802
corpglory-dev <dev@corpglory.com>
2020-05-27 15:13:03 +0300

    test

---
+++ src/ChartwerkLineChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="chartwerk-line-chart" :id="id">
+  </div>
+</template>
+
+<script>
+import ChartwerkBase from '@chartwerk/base/dist/ChartwerkBase.vue';
+
+import { ChartwerkLineChart } from '@chartwerk/line-chart';
+
+import Vue from 'vue';
+
+import * as _ from 'lodash';
+
+export default ChartwerkBase.extend({
+  methods: {
+    render() {
+      new ChartwerkLineChart(document.getElementById(this.id), this.series, this.options);
+    }
+  }
+})
+</script>
+
+<style scoped>
+.vue-chartwerk-base {
+  width: 100%;
+  height: 100%;
+}
+</style>
